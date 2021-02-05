f2546ef4e0754a830afab9557eed1aa227817004
Gerlison <franciscojerlison1@gmail.com>
2021-02-05 19:52:25 -0300

    chore: configure linting

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
