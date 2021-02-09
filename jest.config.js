e49d9770aff750f7c2a82bcfbeb60d71c174609c
Gerlison <franciscojerlison1@gmail.com>
2021-02-09 00:11:42 -0300

    chore: fix jest config file name

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
-export default {
+module.exports = {
   preset: 'jest-expo',
+  setupFiles: ['<rootDir>/jest.setup.js'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
